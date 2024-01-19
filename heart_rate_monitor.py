d82bd1f9a51934a4dc4133c8611319785db563f0
francescoB1997 <francesco.0297@hotmail.it>
2024-01-19 11:21:45 +0100

    Creato test

---
+++ heart_rate_monitor.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time as Time
+
+GPIO_LED = 2
+GPIO_HR = 3
+
+def setup(): 
+    """ setup function """
+    print("setup function")
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(GPIO_LED, GPIO.OUT)
+    GPIO.setup(GPIO_HR, GPIO.IN)
+
+
+setup()
+
+while True:
+    GPIO.output(2, GPIO.HIGH)
+    Time.sleep(1)
+    GPIO.output(2, GPIO.LOW)
+    Time.sleep(1)
+    
